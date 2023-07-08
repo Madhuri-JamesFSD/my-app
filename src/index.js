79de430c524d28820fd8941ca0618370c10183cc
Madhuri-JamesFSD <madhuriblrrmsh@gmail.com>
2023-07-08 22:38:13 +0530

    basics

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>React classes</h1>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+  <section>
+      <h1>List of items</h1>
+    <ul>
+      <li>Cheese</li>
+      <li>Butter</li>
+        <li>Milk</li>
+        <li>Loop logic</li>
+    </ul>
+    </section>
+    <footer>This is a footer</footer>
+    </div>);
+//const root = document.getElementById("root")
+//root.append('Hello');
+//document.body.append(root);
